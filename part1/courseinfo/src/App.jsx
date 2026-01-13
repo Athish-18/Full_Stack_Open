83c43875c74df755972d02e48ec4b6df961f987f
Athish <cathishk@gmail.com>
2026-01-13 19:35:31 +0530

    added 1.3

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,37 +1,78 @@
-const Header = ({ course }) => {
-  return <h1>{course}</h1>;
-};
+// const Header = ({ course }) => {
+//   return <h1>{course}</h1>;
+// };
 
-const Part = ({ name, exercises }) => {
-  return (
-    <p>
-      {name} {exercises}
-    </p>
-  );
-};
+// const Part = ({ name, exercises }) => {
+//   return (
+//     <p>
+//       {name} {exercises}
+//     </p>
+//   );
+// };
 
-const Content = () => {
-  return (
-    <>
-      <Part name="Fundamentals of React" exercises={10} />
-      <Part name="Using props to pass data" exercises={7} />
-      <Part name="State of a component" exercises={14} />
-    </>
-  );
-};
+// const Content = () => {
+//   return (
+//     <>
+//       <Part name="Fundamentals of React" exercises={10} />
+//       <Part name="Using props to pass data" exercises={7} />
+//       <Part name="State of a component" exercises={14} />
+//     </>
+//   );
+// };
 
-const Total = () => {
-  return <p>Number of exercises {10 + 7 + 14}</p>;
-};
+// const Total = () => {
+//   return <p>Number of exercises {10 + 7 + 14}</p>;
+// };
+
+// const App = () => {
+//   return (
+//     <>
+//       <Header course="Half Stack application development" />
+//       <Content />
+//       <Total />
+//     </>
+//   );
+// };
+
+// export default App;
+
+
+
+
+
+
 
 const App = () => {
+  const course = "Half Stack application development";
+  const part1 = {
+    name: "Fundamentals of React",
+    exercises: 10,
+  };
+  const part2 = {
+    name: "Using props to pass data",
+    exercises: 7,
+  };
+  const part3 = {
+    name: "State of a component",
+    exercises: 14,
+  };
+
   return (
     <>
-      <Header course="Half Stack application development" />
-      <Content />
-      <Total />
+      <h1> {course} </h1>
+      <p>
+        {part1.name} {part1.exercises}
+      </p>
+      <p>
+        {part2.name} {part2.exercises}
+      </p>
+      <p>
+        {part3.name} {part3.exercises}
+      </p>
+      <p>
+         Number of exercises {part1.exercises + part2.exercises + part3.exercises}
+      </p>
     </>
   );
 };
-
 export default App;
