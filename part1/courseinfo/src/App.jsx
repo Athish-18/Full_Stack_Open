ab71adbb3d2743e2dd9f1aac17bb3335fc136bb3
Athish <cathishk@gmail.com>
2026-01-16 19:56:40 +0530

    1.13

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,3 +1,35 @@
+// import { useState } from "react";
+
+// const App = () => {
+//   const anecdotes = [
+//     "If it hurts, do it more often.",
+//     "Adding manpower to a late software project makes it later!",
+//     "The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
+//     "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
+//     "Premature optimization is the root of all evil.",
+//     "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
+//     "Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.",
+//     "The only way to go fast, is to go well.",
+//   ];
+
+//   const [selected, setSelected] = useState(0);
+
+//   const handleClick=()=>
+//   {
+//     setSelected(Math.floor(Math.random() * anecdotes.length));
+//     console.log(selected);
+//   }
+
+//   return (
+//     <>
+//     <button onClick={handleClick}>Next Anecdote</button>
+//     <p>{anecdotes[selected]}</p>
+//     </>
+//   )
+// };
+
+// export default App;
+
 import { useState } from "react";
 
 const App = () => {
@@ -13,19 +45,31 @@ const App = () => {
   ];
 
   const [selected, setSelected] = useState(0);
+  const [voted,setVoted]=useState(Array(anecdotes.length).fill(0));
 
-  const handleClick=()=>
-  {
+  const handleClick = () => {
     setSelected(Math.floor(Math.random() * anecdotes.length));
-    console.log(selected);
-  }
+    //console.log(selected);
+  };
 
+  const handleVote=()=>
+  {
+    const copy=[...voted];
+    copy[selected]+=1;
+    setVoted(copy);
+  }
+  
+
+  
+  
   return (
     <>
-    <button onClick={handleClick}>Next Anecdote</button>
-    <p>{anecdotes[selected]}</p>
+      <button onClick={handleClick}>Next Anecdote</button>
+      <button onClick={handleVote}>Vote</button>
+      <p>{anecdotes[selected]}</p>
+       <p>has {voted[selected]} votes</p>
     </>
-  )
+  );
 };
 
 export default App;
